d4974d02955775bda8c595c3c91f3fde2e33ed56
KrysTheMango <krysys408@gmail.com>
2025-10-21 14:44:11 +0000

    Deploying to gh-pages from @ KrysTheMango/Github@85435935355bce0ec8a54e434d1135e740f6aa83 🚀

---
+++ js/index.js
@@ -0,0 +1,35 @@
+//Aquest fitxer JS conté el codi per a la pàgina principal index.html
+/*Ara farem 
+el nostre codi*/
+
+var name = "Edu";
+var age = 15;
+const nombre = "Andrea";
+
+console.log(age);
+
+const div = 5 / 3;
+const modul = 5 % 3;
+
+console.log(div);
+console.log(modul);
+
+var num = 10;
+num = num + 1;
+console.log(num);
+num += 1;
+console.log(num);
+num++;
+console.log(num);
+
+num = num - 1;
+console.log(num);
+num -= 1;
+console.log(num);
+num--;
+console.log(num);
+
+const zona = 'Mancha';
+const libro = "En un lugar de la" + zona + "cuyo nombre no me quiero acordar...";
+
+console.log(libro);
